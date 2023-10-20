209f553119a4aec8a027edfe29abf3b5f9bb0f46
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-10-20 22:38:25 +0700

    Set connection to db mysql

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const express = require('express')
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import mysql from 'mysql2'
+import { dbConfig } from './db.js'
+
+dotenv.config()
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+app.use(cors())
+
+const test = (req, callback) => {
+  const db = mysql.createConnection(dbConfig)
+  db.query(
+    'SELECT * FROM users',
+    (err, result) => {
+      if (err) {
+        callback(err)
+      } else {
+        const row = result
+        callback(null, result)
+      }
+      db.end()
+    }
+  )
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    test(req, (err, resp) => {
+      if (err) return res.status(500).json(err)
+      else return res.status(200).json(resp)
+    })
 })
 
 app.listen(port, () => {
