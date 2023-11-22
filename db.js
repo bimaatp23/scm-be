40cf92cee8ad68e07581c5a4a5f7db0299f9ff64
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-11-22 14:41:37 +0700

    Update mysql port

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ dotenv.config()
 
 export const dbConfig = {
     host: process.env.DB_HOST,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
